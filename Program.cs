7e845815deaa043e88da0919aa25ee13a5dab01c
Andrei Pana <andrei.pana@leica-geosystems.com>
2024-03-30 23:54:50 +0100

    Pull individual files

--- Program.cs
+++ Program.cs
@@ -1,28 +1,70 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text;
 
 Console.WriteLine("Quick DVC");
 
-
-//const string path = @"c:\work\infinity.2nd\Data\Projects\rtc\low_40scans_no_images-Vist_AutoBlackAndWhiteTarget";
-//Directory.EnumerateFiles(path, "*.dvc", SearchOption.AllDirectories)
-//    .ToList()
-//    .ForEach(dvc => Console.WriteLine(dvc));
-
 var user = Environment.GetEnvironmentVariable("ARTIFACTORY_USERNAME");
 var pass = Environment.GetEnvironmentVariable("ARTIFACTORY_TOKEN");
 
-const string md5 = "2bd0312ed1d8c8e9c718029a4940fb3c";
-var client = new HttpClient();
-// add credentials to the client
-client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{user}:{pass}")));
-string dlpath = $"https://artifactory.hexagon.com/artifactory/gsurv-generic-release-local/sprout/testdata/files/md5/{md5.Substring(0, 2)}/{md5.Substring(2)}";
-Console.WriteLine(dlpath);
-var response = await client.GetAsync(dlpath);
+//var sw = Stopwatch.StartNew();
+//const string path = @"c:\work\infinity.2nd\Data\Projects\rtc\low_40scans_no_images-Vist_AutoBlackAndWhiteTarget";
+//const string path = @"c:\work\infinity.2nd\Data\Projects\rtc";
+//const string path = @"c:\work\infinity.2nd\Data\Projects\";
+//const string path = @"c:\work\infinity.2nd\Data\";
+//var files = Directory.EnumerateFiles(path, "*.dvc", SearchOption.AllDirectories)
+//    .Take(10)
+//    .ToList();
+////files.ForEach(dvc => Console.WriteLine(dvc));
+//Console.WriteLine(files.Count);
+//Console.WriteLine(sw.Elapsed);
 
-using (var fs = new FileStream("test.db", FileMode.Create, FileAccess.Write))
+const string dvcFilePath = @"c:\work\infinity.2nd\Data\Projects\rtc\low_40scans_no_images-Vist_AutoBlackAndWhiteTarget\da2ada28-06eb-4844-a14e-2d3ff16e665f\project_version_1647868925000.db.dvc";
+await PullDvcFile(dvcFilePath);
+
+return;
+
+async Task PullDvcFile(string dvcFilePath)
 {
+    var md5 = await ReadHashFromDvcFile(dvcFilePath);
+    if (md5 == null)
+    {
+        Console.WriteLine($"Failed to read hash from {dvcFilePath}");
+        return;
+    }
+
+    using var client = new HttpClient();
+
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{user}:{pass}")));
+    string url = $"https://artifactory.hexagon.com/artifactory/gsurv-generic-release-local/sprout/testdata/files/md5/{md5.Substring(0, 2)}/{md5.Substring(2)}";
+    //Console.WriteLine(url);
+    var response = await client.GetAsync(url);
+    var targetFilePath = dvcFilePath.Substring(0, dvcFilePath.Length - 4);
+    //Console.WriteLine(targetFilePath);
+
+    using var fs = new FileStream(targetFilePath, FileMode.Create, FileAccess.Write);
+    
     await response.Content.CopyToAsync(fs);
+
+    Console.WriteLine(response.StatusCode);
 }
 
-Console.WriteLine(response.StatusCode);
+async Task<string?> ReadHashFromDvcFile(string dvcFilePath)
+{
+    try
+    {
+        var dvcFileContent = await File.ReadAllTextAsync(dvcFilePath);
+        return ReadHashFromDvcFileContent(dvcFileContent);
+    }
+    catch (Exception)
+    {
+        return null;
+    }
+}
+
+string? ReadHashFromDvcFileContent(string dvcFileContent)
+{
+    const string marker = "md5: ";
+    var index = dvcFileContent.IndexOf(marker) + marker.Length;
+    return dvcFileContent.Substring(index, 32);
+}
