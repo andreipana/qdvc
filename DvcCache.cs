0d44a72303f2bb536b0bde8941f4dfe9a24a1c0b
Andrei Pana <andrei.pana@leica-geosystems.com>
2024-03-31 13:58:30 +0200

    Use dvc cache for caching

---
+++ DvcCache.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qdvc
+{
+    internal class DvcCache
+    {
+        public string DvcCacheFolder { get; }
+        private string DvcCacheFilesFolder { get; }
+
+        public DvcCache(string dvcCacheFolder)
+        {
+            DvcCacheFolder = dvcCacheFolder;
+            DvcCacheFilesFolder = Path.Combine(DvcCacheFolder, "files", "md5");
+            EnsureFolderStructureExists();
+        }
+
+        private void EnsureFolderStructureExists()
+        {
+            var CreateDirectoryIfNeeded = (string path) =>
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+            };
+
+            var sw = Stopwatch.StartNew();
+            CreateDirectoryIfNeeded(Path.Combine(DvcCacheFolder, "files"));
+            CreateDirectoryIfNeeded(DvcCacheFilesFolder);
+            for (var i = 0; i < 256; i++)
+                CreateDirectoryIfNeeded(Path.Combine(DvcCacheFilesFolder, i.ToString("X2")));
+            Console.WriteLine($"Ensuring folder structure took {sw.Elapsed}");
+        }
+
+        internal static DvcCache? GetDvcCacheForFolder(string path)
+        {
+            var dvcCacheFolder = FindDvcCacheFolder(path);
+            if (dvcCacheFolder == null)
+                return null;
+
+            return new DvcCache(dvcCacheFolder);
+        }
+
+        private static string? FindDvcCacheFolder(string path)
+        {
+            string? directory = null;
+            if (File.Exists(path))
+                directory = Path.GetDirectoryName(path);
+            else if (Directory.Exists(path))
+                directory = path;
+
+            while (directory != null)
+            {
+                var dvcFolder = Path.Combine(directory, ".dvc");
+                if (Directory.Exists(dvcFolder))
+                    return Path.Combine(dvcFolder, "cache");
+
+                directory = Path.GetDirectoryName(directory);
+            }
+
+            return null;
+        }
+
+        public bool ContainsFile(string md5)
+        {
+            return File.Exists(GetCacheFilePath(md5));
+        }
+
+        public string GetCacheFilePath(string md5)
+        {
+            return Path.Combine(DvcCacheFilesFolder, md5.Substring(0, 2), md5.Substring(2));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using qdvc;
+using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Text;
 
@@ -9,11 +10,19 @@ var pass = Environment.GetEnvironmentVariable("ARTIFACTORY_TOKEN");
 
 var sw = Stopwatch.StartNew();
 
+
 //const string path = @"c:\work\infinity.2nd\Data\Projects\rtc\low_40scans_no_images-Vist_AutoBlackAndWhiteTarget";
-const string path = @"c:\work\infinity.2nd\Data\Projects\rtc\RTC360_4skany_AutoBlackAndWhiteTarget";
-//const string path = @"c:\work\infinity.2nd\Data\Projects\rtc";
+//const string path = @"c:\work\infinity.2nd\Data\Projects\rtc\RTC360_4skany_AutoBlackAndWhiteTarget";
+const string path = @"c:\work\infinity.2nd\Data\Projects\rtc";
 //const string path = @"c:\work\infinity.2nd\Data\Projects\";
 //const string path = @"c:\work\infinity.2nd\Data\";
+
+var dvcCache = DvcCache.GetDvcCacheForFolder(path);
+//Console.WriteLine($"DVC cache folder: {dvcCache.DvcCacheFolder}");
+//Console.WriteLine(dvcCache.ContainsFile("f41ba0b6951431cf2107e36a0d8d34fa"));
+//Console.WriteLine(dvcCache.GetCacheFilePath("f41ba0b6951431cf2107e36a0d8d34fa"));
+
+//return;
 
 var files = Directory.EnumerateFiles(path, "*.dvc", SearchOption.AllDirectories);
 var options = new ParallelOptions
@@ -32,7 +41,7 @@ return;
 
 async Task PullDvcFile(string dvcFilePath)
 {
-    Console.WriteLine($"Pulling {dvcFilePath}");
+    Console.WriteLine($"Pull     {dvcFilePath}");
     var md5 = await ReadHashFromDvcFile(dvcFilePath);
     if (md5 == null)
     {
@@ -40,20 +49,45 @@ async Task PullDvcFile(string dvcFilePath)
         return;
     }
 
+    var targetFilePath = dvcFilePath.Substring(0, dvcFilePath.Length - 4);
+
+    if (dvcCache != null)
+    {
+        var cacheFilePath = dvcCache.GetCacheFilePath(md5);
+
+        if (!dvcCache.ContainsFile(md5))
+        {
+            await DownloadFileAsync(md5, cacheFilePath);
+            new FileInfo(cacheFilePath).Attributes |= FileAttributes.ReadOnly;
+        }
+
+        {
+            File.Copy(cacheFilePath, targetFilePath, true);
+            new FileInfo(targetFilePath).Attributes &= ~FileAttributes.ReadOnly;
+        
+            Console.WriteLine($"Pulled   {dvcFilePath} FROM CACHE");
+        }
+    }
+    else
+    {
+        await DownloadFileAsync(md5, targetFilePath);
+    }
+}
+
+async Task DownloadFileAsync(string md5, string filePath)
+{
     using var client = new HttpClient();
 
     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{user}:{pass}")));
     string url = $"https://artifactory.hexagon.com/artifactory/gsurv-generic-release-local/sprout/testdata/files/md5/{md5.Substring(0, 2)}/{md5.Substring(2)}";
     //Console.WriteLine(url);
     var response = await client.GetAsync(url);
-    var targetFilePath = dvcFilePath.Substring(0, dvcFilePath.Length - 4);
     //Console.WriteLine(targetFilePath);
 
-    using var fs = new FileStream(targetFilePath, FileMode.Create, FileAccess.Write);
-
+    using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
     await response.Content.CopyToAsync(fs);
 
-    Console.WriteLine($"Pulled  {dvcFilePath} {response.StatusCode}");
+    Console.WriteLine($"Download {filePath} {response.StatusCode}");
 }
 
 async Task<string?> ReadHashFromDvcFile(string dvcFilePath)
